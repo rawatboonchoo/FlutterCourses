404076a94197e0d6e6ee55c2440bc5cfbb72d227
rawat boonchoo <rawat boonchoo.rawat@gmail.com>
2020-11-12 11:07:08 +0700

    get Detail to database by FutureBuilder

--- flutterapp/lib/pages/product_stack/detail_page.dart
+++ flutterapp/lib/pages/product_stack/detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+//import convert Decode Json
+import 'dart:convert';
+//import http
+import 'package:http/http.dart' as http;
 
 class DetailPage extends StatefulWidget {
   DetailPage({Key key}) : super(key: key);
@@ -8,13 +12,94 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  Map<String, dynamic> product; //สร้างตัวแปร Map เพื่อรับข้อมูล
+  //ใช้ Dynamic รับข้อมูล จาก Database api
+  Map<String, dynamic> detail;
+
+  Future<dynamic> getData(int id) async {
+    var url = 'https://api.codingthailand.com/api/course/$id';
+    var response = await http.get(url);
+    //print(response.body); //ดูข้อมูล
+    //response เป็น String ต้องแปลงเป็น Dart เพื่อใช้งาน
+    //Decode String to Json
+    //print(json.decode(response.body));
+    //นำข้อมูล json ไปใส่ในตัวแปร detail ที่สร้างไว้
+    if (response.statusCode == 200) {
+      //check response.statusCode == 200
+      detail = json.decode(response.body);
+
+      //ตัวอย่างข้อมูล
+      //   {
+      // "status": "success",
+      // "status_code": 200,
+      // "data": [
+      //     {
+      //         "ch_id": 60,
+      //         "course_id": 1,
+      //         "ch_title": "ตอนที่ 1 ทำความรู้จักกับ HTML",
+      //         "ch_dateadd": "2014-12-09",
+      //         "ch_timetotal": "7:44",
+      //         "ch_view": 14637,
+      //         "ch_url": "bEWqFE8rqKA"
+      //     },
+      // print(detail['data']);
+      // print(detail['status']);
+      // print(detail['status_code']);
+      //print('load data scuucessed');
+      return detail;
+    } else {
+      //print('error from backend status cide 400/500');
+      throw Exception('error from backend status cide 400/500');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    product = ModalRoute.of(context)
+        .settings
+        .arguments; //ModalRoute.of(context).settings.arguments;
+    //getData(product['id']);
     return Scaffold(
       appBar: AppBar(
-        title: Text('รายละเอียดสินค้า'),
+        title: Text('${product['title']}'),
       ),
-      body: Text('รายละเอียดสินค้า'),
+      body: FutureBuilder<dynamic>(
+          //การใช้งาน FutureBuilder เพื่อดึงขอมูล getData
+          future: getData(product['id']),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) //snapshot.hasData มีข้อมูล
+            {
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        '${snapshot.data['data'][index]['ch_title']}'), //snapshot.data['data'][index]['ch_title'] |snapshot คือข้อมูลทั้งหมด | data ข้มูลที่ return มา | ['data'] คือ Key data |
+                    subtitle:
+                        Text('${snapshot.data['data'][index]['ch_dateadd']}'),
+                    trailing: Chip(
+                      label: Text(
+                        '${snapshot.data['data'][index]['ch_view']}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      backgroundColor: Colors.pink,
+                    ),
+                  );
+                }, //โชว์รายการ
+                separatorBuilder: (context, index) =>
+                    //Divider(), //เส้นขีดขั้นจะใช้แสดงอะไร
+                    SizedBox(
+                  //ระยะห่างขั้นจะใช้แสดงอะไร
+                  height: 30,
+                ),
+                itemCount: snapshot.data['data'].length, //จำนวน array
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                  child: Text('เกิดข้อผิดพลาดจาก Server ${snapshot.error}'));
+            }
+
+            return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
--- flutterapp/lib/pages/product_stack/product_page.dart
+++ flutterapp/lib/pages/product_stack/product_page.dart
@@ -17,6 +17,7 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   //สร้างตัวแปรมารับข้อมูลมทราเป็น arra
   List<Data> data = [];
+  bool isLoading = true; //ทำโหลดก่อนดึงดาต้า
 
   //ดึงข้อมูลมาจาก Database ข้อมูลต้องเป็นรูปแบบ json
   Future<void> getData() async {
@@ -27,12 +28,23 @@ class _ProductPageState extends State<ProductPage> {
     //Decode String to Json
     //print(json.decode(response.body));
     //นำข้อมูล json ไปใส่ใน model ที่สร้างไว้
-    final Product product = Product.fromJson(json.decode(response.body));
-    //print(product.data);
-    setState(() {
-      //อย่าลืม setStat()
-      data = product.data;
-    });
+    if (response.statusCode == 200) {
+      //check response.statusCode == 200
+      final Product product = Product.fromJson(json.decode(response.body));
+      //print(product.data);
+      setState(() {
+        //อย่าลืม setStat()
+        data = product.data;
+        isLoading = false;
+      });
+      print('load data scuucessed');
+    } else {
+      setState(() {
+        //อย่าลืม setStat()
+        isLoading = false;
+      });
+      print('error from backend status cide 400/500');
+    }
   }
 
   @override
@@ -45,7 +57,6 @@ class _ProductPageState extends State<ProductPage> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     print('dispose (product page)');
     super.dispose();
   }
@@ -58,54 +69,59 @@ class _ProductPageState extends State<ProductPage> {
       appBar: AppBar(
         title: Text('Product Page'),
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, 'productstack/detail');
-            },
-            leading: Container(
-              height: 80,
-              width: 80,
-              decoration: BoxDecoration(
-                  color: const Color(0xff7c94b6),
-                  shape: BoxShape.circle, //รูปทรง กลม / เหลี่ยม
-                  image: DecorationImage(
-                    image: NetworkImage(data[index].picture),
-                    fit: BoxFit.cover,
+      body: isLoading == true
+          ? Center(child: CircularProgressIndicator()) //loading
+          : ListView.separated(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    Navigator.pushNamed(context, 'productstack/detail',arguments: {
+                      'id': data[index].id,
+                      'title' : data[index].title
+                      }); //ส่ง arguments เป็น map ทางฝังรับต้องรับเป็น map
+                  },
+                  leading: Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff7c94b6),
+                        shape: BoxShape.circle, //รูปทรง กลม / เหลี่ยม
+                        image: DecorationImage(
+                          image: NetworkImage(data[index].picture),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(
+                          color: Colors.white10,
+                          width: 1,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 1), // changes position of shadow
+                          ),
+                        ]),
                   ),
-                  border: Border.all(
-                    color: Colors.white10,
-                    width: 1,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 1), // changes position of shadow
+                  title: Text('${data[index].title}'),
+                  subtitle: Text('${data[index].detail}'),
+                  trailing: Chip(
+                    label: Text(
+                      '${data[index].view}',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ]),
-            ),
-            title: Text('${data[index].title}'),
-            subtitle: Text('${data[index].detail}'),
-            trailing: Chip(
-              label: Text(
-                '${data[index].view}',
-                style: TextStyle(color: Colors.white),
+                    backgroundColor: Colors.pink,
+                  ),
+                );
+              }, //โชว์รายการ
+              separatorBuilder: (context, index) =>
+                  //Divider(), //เส้นขีดขั้นจะใช้แสดงอะไร
+                  SizedBox(
+                //ระยะห่างขั้นจะใช้แสดงอะไร
+                height: 30,
               ),
-              backgroundColor: Colors.pink,
+              itemCount: data.length, //จำนวน array
             ),
-          );
-        }, //โชว์รายการ
-        separatorBuilder: (context, index) =>
-            //Divider(), //เส้นขีดขั้นจะใช้แสดงอะไร
-            SizedBox(
-          //ระยะห่างขั้นจะใช้แสดงอะไร
-          height: 30,
-        ),
-        itemCount: data.length, //จำนวน array
-      ),
     );
   }
 }
