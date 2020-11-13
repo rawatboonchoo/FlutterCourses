119471084b7d4e09f8652fbaa615e3f6f46459ae
rawat boonchoo <rawat boonchoo.rawat@gmail.com>
2020-11-13 13:30:37 +0700

    add redux main

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -6,9 +6,14 @@ import 'package:flutterapp/pages/home_stack/home_stack.dart';
 import 'package:flutterapp/pages/login_page.dart';
 import 'package:flutterapp/pages/product_stack/product_stack.dart';
 import 'package:flutterapp/pages/register_page.dart';
+import 'package:flutterapp/redux/app_reducer.dart';
 
 //shared_preferences //ไว้สำหรับเก็บข้อมูลในเครื่องรูปแบบ Key Value
 import 'package:shared_preferences/shared_preferences.dart';
+
+//redux
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 String token;
 
@@ -21,45 +26,53 @@ void main() async {
   //เมื่อมี await ต้องมี async ใน function เสมอ
   SharedPreferences prefs = await SharedPreferences.getInstance();
   token = prefs.getString('token');
-  print(token);
-  
-  runApp(MyApp());
+  //print(token);
+
+//Reducer
+  final myStore = Store<AppState>(appReducer, initialState: AppState.inital());
+
+  runApp(MyApp(store: myStore));
 }
 
 class MyApp extends StatelessWidget {
+  final Store<AppState> store;
+  MyApp({this.store}); //this.store = myStore
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primaryColor: Color(0xff78002e),
-          //primarySwatch: Colors.deepOrange, //เปลี่ยนสีทั้ง app
-          //primaryColor: Colors.deepOrange[800],//primaryColor สามารถใส่น้ำหนักสีได้ ทั้ง app | อยากใส่สีเอง เรียก class Color(oxffตามด้วยรหัสสี) | https://material.io/resources/color/#!/?view.left=0&view.right=0
-          canvasColor: Colors.white, //สี blackground
-          accentColor: Colors.deepPurple, // สีของ ปุม
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+    return StoreProvider(
+        store: store,
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+              primaryColor: Color(0xff78002e),
+              //primarySwatch: Colors.deepOrange, //เปลี่ยนสีทั้ง app
+              //primaryColor: Colors.deepOrange[800],//primaryColor สามารถใส่น้ำหนักสีได้ ทั้ง app | อยากใส่สีเอง เรียก class Color(oxffตามด้วยรหัสสี) | https://material.io/resources/color/#!/?view.left=0&view.right=0
+              canvasColor: Colors.white, //สี blackground
+              accentColor: Colors.deepPurple, // สีของ ปุม
+              visualDensity: VisualDensity.adaptivePlatformDensity,
 
-          //ข้อความ เปลี่ยนทั้ง app ที่มีการเรียกใช้ headline1
-          textTheme: TextTheme(
-              headline1: TextStyle(
-                  fontSize: 30,
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold))),
-      debugShowCheckedModeBanner: false, //เอา Debug ออก
-      //home: MyHomePage(title: 'Flutter Mobile Framework'), เนื่องจากจะใช้ initialRount แทน
-      initialRoute: '/', //การสร้างลิงค์สำหรับเปิดหน้า
-      //groub rout
-      routes: {   
-        // '/': (context) =>  LoginPage(), //หน้าแรก     
-        '/': (context) =>  token == null ? LoginPage() : HomeStack(), //check login
-        '/register': (context) => RegisterPage(), //หน้าสมัครสมาชิก
-        //'/': (context) =>HomeStack(), //ไม่ใส่ title เพราะมีรูปภาพแล้ว | หน้าแรกจะใช้ '/'
-        '/homestack': (context) => HomeStack(),
-        '/productstack': (context) => ProductStack(),
-        //'/': (context) => FirePage(), //ทดสอบ
-        '/login': (context) => LoginPage(),
-      },
-    );
+              //ข้อความ เปลี่ยนทั้ง app ที่มีการเรียกใช้ headline1
+              textTheme: TextTheme(
+                  headline1: TextStyle(
+                      fontSize: 30,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold))),
+          debugShowCheckedModeBanner: false, //เอา Debug ออก
+          //home: MyHomePage(title: 'Flutter Mobile Framework'), เนื่องจากจะใช้ initialRount แทน
+          initialRoute: '/', //การสร้างลิงค์สำหรับเปิดหน้า
+          //groub rout
+          routes: {
+            // '/': (context) =>  LoginPage(), //หน้าแรก
+            '/': (context) =>
+                token == null ? LoginPage() : HomeStack(), //check login
+            '/register': (context) => RegisterPage(), //หน้าสมัครสมาชิก
+            //'/': (context) =>HomeStack(), //ไม่ใส่ title เพราะมีรูปภาพแล้ว | หน้าแรกจะใช้ '/'
+            '/homestack': (context) => HomeStack(),
+            '/productstack': (context) => ProductStack(),
+            //'/': (context) => FirePage(), //ทดสอบ
+            '/login': (context) => LoginPage(),
+          },
+        ));
   }
 }
