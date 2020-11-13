4adef8590e16b1306bba16bbba0fe25d68b0b866
rawat boonchoo <rawat boonchoo.rawat@gmail.com>
2020-11-13 15:12:07 +0700

    add google map complate

--- flutterapp/lib/pages/home_stack/map_page.dart
+++ flutterapp/lib/pages/home_stack/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key}) : super(key: key);
@@ -8,13 +10,30 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Map'),
       ),
-      body: Text('map loading...'),
+      body: GoogleMap(
+        //ปักมุด markers รับข้อมูลเป็น Set
+        markers: <Marker>[
+          Marker(
+              markerId: MarkerId('001'),
+              position: LatLng(13.80564244, 100.574134),
+              infoWindow:
+                  InfoWindow(title: 'ทดสอบ', snippet: '55555', onTap: () {}))
+        ].toSet(),
+        //โชว์ google map ตาม LatLng(13.80564244, 100.574134)
+        initialCameraPosition:
+            CameraPosition(target: LatLng(13.80564244, 100.574134), zoom: 16),
+        mapType: MapType.normal,
+        onMapCreated: (GoogleMapController controler) {
+          _controller.complete(controler);
+        },
+      ),
     );
   }
 }
